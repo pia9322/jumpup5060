d6106aebb9471a6c2317820d428bb3cdad6e5918
pia9322 <77098879+pia9322@users.noreply.github.com>
2021-08-12 20:12:17 +0900

    upload
    upload

---
+++ js/index.js
@@ -0,0 +1,7 @@
+// $(function(){
+//     $('.header_wrap > ul > li, .header_bg').hover(function(){
+//         $(".header_wrap ul ul, .header_bg").stop().slideDown(150)
+//     },function(){
+//         $(".header_wrap ul ul, .header_bg").stop().slideUp(150)
+//     })
+// });
