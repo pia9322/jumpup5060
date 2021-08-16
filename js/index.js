e5195bc9bc5a67b86be562dff21a098dae9c8e9d
pia9322 <77098879+pia9322@users.noreply.github.com>
2021-08-17 08:55:12 +0900

    ver 1.0.0 reload
    header hover add

--- js/index.js
+++ js/index.js
@@ -58,25 +58,24 @@ $(function(){
 
 
 
-    var cnt0 = 0;
+    // var cnt0 = 0;
 
-    counterFn(".number_01",256);
-    counterFn(".number_02",56.3);
-    counterFn(".number_03",80);
+    // counterFn(".number_01",256);
+    // counterFn(".number_02",56.3);
+    // counterFn(".number_03",80);
 
-    function counterFn(targer,number) {
-
-        id0 = setInterval(count0Fn, 10);
-
-        function count0Fn() {
-        cnt0++;
-        if (cnt0 > number) {
-            clearInterval(id0);
-        } else {
-            $(targer).text(cnt0);
-        }
-        }
-    }
+    // function counterFn(targer,number) {
+    //     id0 = setInterval(count0Fn, 10);
+        
+    //     function count0Fn() {
+    //     cnt0++;
+    //     if (cnt0 > number) {
+    //         clearInterval(id0);
+    //     } else {
+    //         $(targer).text(cnt0);
+    //     }
+    //     }
+    // }
 
 
 
