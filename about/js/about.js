5c565350cd1487b50a05c8aa900fe450be6474d8
pia9322 <77098879+pia9322@users.noreply.github.com>
2021-08-18 10:47:51 +0900

    reload

--- about/js/about.js
+++ about/js/about.js
@@ -7,25 +7,26 @@ $(function(){
     // header footer init
 
 
+    var countOptions = {
+        suffix: "%"  // 뒤에 표시될 텍스트
+      };
+      
  
   
     const countUp1 = new CountUp("counting_01", 0, 239, 0 , 3);
-    const countUp2 = new CountUp("counting_02", 0, 56.3, 1, 3);
+    const countUp2 = new CountUp("counting_02", 0, 748, 0, 3);
     const countUp3 = new CountUp("counting_03", 0, 239, 0 , 3);
-    const countUp4 = new CountUp("counting_04", 0, 80, 0 , 3);
+    const countUp4 = new CountUp("counting_04", 0, 80, 0 , 3, countOptions);
     const countUp5 = new CountUp("counting_05", 0, 56.3, 1 , 3);
     
     console.log($('.countTextBox_01').offset().top);
     
+    
     $(window).scroll(function(){
       let scrollTop = $(window).scrollTop();
       let countTop01 = $('.countTextBox_01').position().top- 400 ;
-      let countTop01Pos = $('.countTextBox_01').position().top ;
       let countTop02 = $('.countTextBox_02').position().top - 400 ;
 
-      console.log(scrollTop, countTop01, countTop01Pos);
-    //   console.log(countTop01Pos, countTop01);
-  
         if (scrollTop >= countTop01) {
             countUp1.start();
             countUp2.start();
