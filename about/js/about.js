95edf82dea7a39d5361fecbed7c13f4cbef28975
pia9322 <77098879+pia9322@users.noreply.github.com>
2021-08-19 14:44:40 +0900

    ver 1.3 (info_01_02 add)

--- about/js/about.js
+++ about/js/about.js
@@ -1,10 +1,5 @@
 $(function(){
-    // header footer init
-    $('header').load('init.html header .header_wrap', init)
-    $('footer').load('init.html footer .footer')
-
-    function init() {}
-    // header footer init
+   
 
 
     var countOptions = {
---
+++ about/js/init.js
@@ -0,0 +1,9 @@
+$(function(){
+     // header footer init
+     $('header').load('init.html header .header_wrap', init)
+     $('footer').load('init.html footer .footer')
+     $('.header_background_menu').load('init.html .header_background_menu')
+ 
+     function init() {}
+     // header footer init
+});
