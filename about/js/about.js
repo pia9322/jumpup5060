bd319ddbf47cbda7ab559e6d7550254db1eb8f40
pia9322 <77098879+pia9322@users.noreply.github.com>
2021-08-23 11:25:28 +0900

    ver 2.0 (media 767px fixed)

--- about/js/about.js
+++ about/js/about.js
@@ -1,39 +1,42 @@
-$(function(){
-   
+$(function () {
 
 
-    var countOptions = {
-        suffix: "%"
-      };
-      
- 
-  
-    const countUp1 = new CountUp("counting_01", 0, 239, 0 , 3);
-    const countUp2 = new CountUp("counting_02", 0, 748, 0, 3);
-    const countUp3 = new CountUp("counting_03", 0, 239, 0 , 3);
-    const countUp4 = new CountUp("counting_04", 0, 80, 0 , 3, countOptions);
-    const countUp5 = new CountUp("counting_05", 0, 56.3, 1 , 3);
-    
-    console.log($('.countTextBox_01').offset().top);
-    
-    
-    $(window).scroll(function(){
-      let scrollTop = $(window).scrollTop();
-      let countTop01 = $('.countTextBox_01').position().top - 500 ;
-      let countTop02 = $('.countTextBox_02').position().top - 500 ;
 
-        if (scrollTop >= countTop01) {
-            countUp1.start();
-            countUp2.start();
-        }
+  var countOptions = {
+    suffix: "%"
+  };
 
-        if (scrollTop >= countTop02) {
-            countUp3.start();
-            countUp4.start();
-            countUp5.start();
-        }
-   
-    });
+
+
+  const countUp1 = new CountUp("counting_01", 0, 239, 0, 3);
+  const countUp2 = new CountUp("counting_02", 0, 748, 0, 3);
+  const countUp3 = new CountUp("counting_03", 0, 239, 0, 3);
+  const countUp4 = new CountUp("counting_04", 0, 80, 0, 3, countOptions);
+  const countUp5 = new CountUp("counting_05", 0, 56.3, 1, 3);
+
+  console.log($('.countTextBox_01').offset().top);
+
+
+  $(window).scroll(function () {
+    let scrollTop = $(window).scrollTop();
+    let countTop01 = $('.countTextBox_01').position().top - 500;
+    let countTop02 = $('.countTextBox_02').position().top - 500;
+
+    if (scrollTop >= countTop01) {
+      countUp1.start();
+      countUp2.start();
+    }
+
+    if (scrollTop >= countTop02) {
+      countUp3.start();
+      countUp4.start();
+      countUp5.start();
+    }
+
+  });
+
+
+
 
 
 
--- about/js/init.js
+++ about/js/init.js
@@ -1,9 +1,55 @@
-$(function(){
+$(function () {
      // header footer init
      $('header').load('init.html header .header_wrap', init)
      $('footer').load('init.html footer .footer')
-     $('.header_background_menu').load('init.html .header_background_menu')
- 
-     function init() {}
+     $('.header_background_menu').load('init.html .header_background_menu > ul')
+
+     function init() { }
      // header footer init
+
+
+
+     // function NavScroll() {
+     //      if ($('.header_background_menu').hasClass('active')) {
+     //           $('body').css({ 'overflow': 'hidden' });
+     //      } else {
+     //           $('body').css({ 'overflow': 'auto' });
+     //      };
+     // }
+
+
+     // function Navopen() {
+     //      $('.header_right').on('click', function () {
+     //           $('.header_background_menu').toggleClass('active');
+
+     //           if ($('.header_background_menu').hasClass('active')) {
+     //                $('.header_background_menu').css({ "cursor": "alias" });
+     //           }
+     //           NavScroll();
+     //      });
+     // }
+     // Navopen()
+
+
+     // function mNav() {
+     //      let moNavBack = document.querySelector('.header_background_menu');
+     //      moNavBack.addEventListener('click', function (e) {
+     //           if (e.target.nodeName != 'A') {
+     //                $('.header_background_menu').removeClass('active')
+     //           }
+     //           NavScroll();
+     //      });
+     // }
+     // mNav();
+
+     // console.log('asd')
+
+
+     // $(window).on('click',function(e){
+     //      console.log(e.target)
+     // });
+
+
+
+
 });
