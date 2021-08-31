ccaaafa6e9501df52d172ef2db1dc146538be02b
pia9322 <77098879+pia9322@users.noreply.github.com>
2021-08-31 15:32:57 +0900

    scroll remove / init_js, index_js modify

--- about/js/init.js
+++ about/js/init.js
@@ -11,34 +11,48 @@ $(function () {
           
           function NavScroll() {
                if ($('.header_background_menu').hasClass('active')) {
-                    $('body').css({ 'overflow': 'hidden' });
+                   $('body').css({ 'overflow': 'hidden' });
                } else {
-                    $('body').css({ 'overflow': 'auto' });
+                   $('body').css({ 'overflow': 'auto' });
                };
-          }
-
-          function Navopen() {
+           }
+       
+       
+       
+           function Navopen() {
                $('.header_right').on('click', function () {
-                    $('.header_background_menu').toggleClass('active');
-     
-                    if ($('.header_background_menu').hasClass('active')) {
-                         $('.header_background_menu').css({ "cursor": "alias" });
-                    }
-                    NavScroll();
+                   $('.header_background_menu').toggleClass('active');
+                   $('header').toggleClass('active');
+                   $('.header_right').toggleClass('active');
+       
+                   if ($('.header_background_menu').hasClass('active')) {
+                       $('.header_background_menu').css({ "cursor": "alias" });
+                   }
+                   NavScroll();
+       
                });
-          }
-          Navopen()
-
-          function mNav() {
+       
+           }
+           Navopen()
+       
+       
+           function mNav() {
                let moNavBack = document.querySelector('.header_background_menu');
+       
                moNavBack.addEventListener('click', function (e) {
-                    if (e.target.nodeName != 'A') {
-                         $('.header_background_menu').removeClass('active')
-                    }
-                    NavScroll();
+       
+                   if (e.target.nodeName != 'A') {
+                       $('.header_background_menu').removeClass('active')
+                       $('header').removeClass('active');
+                       $('.header_right').removeClass('active');
+       
+                   }
+                   NavScroll();
+       
                });
-          }
-          mNav();
+       
+           }
+           mNav();
 
 
           // setTimeout(() => {
